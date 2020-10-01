7906b03fef235e80a12616156e86a482ad4f8e42
Vik Birdi <lifeasvik@gmail.com>
2020-10-01 09:34:38 -0500

    setup react and added routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Route, Switch } from "react-router-dom";
+import Home from "./components/Home.js";
+import Portfolio from "./components/Portfolio.js";
+import About from "./components/About.js";
+import Contact from "./components/Contact.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+        <Switch>
+          <main>
+            <Route exact path={"/"} component={Home} />{" "}
+            <Route path={"/portfolio"} component={Portfolio} />{" "}
+            <Route path={"/about"} component={About} />{" "}
+            <Route path={"/contact"} component={Contact} />{" "}
+          </main>
+        </Switch>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+<div>
+<ul className="navbar">
+    <li><Link to={'/'} className="nav-link"> Home </Link></li>
+    <li><Link to={'/portfolio'} className="nav-link">Portfolio</Link></li>
+    <li><Link to={'/about'} className="nav-link">About</Link></li>
+    <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
+  </ul>
+</div>
