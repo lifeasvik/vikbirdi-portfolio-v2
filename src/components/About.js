d12e4f509a3f6f76d4d866db1c7a6d90d781c137
Vik Birdi <lifeasvik@gmail.com>
2020-10-08 22:20:09 -0500

    added about and projects still working on wireframe

--- src/components/About.js
+++ src/components/About.js
@@ -10,10 +10,11 @@ class About extends Component {
                     <div><h1>About</h1></div>
                 </div>
                 <div className="pagePadder">
-                    <div className="mainSplash">Splash Content Here</div>
+                    <div className="mainSplash">Meet Vik</div>
 
                 </div>
-                <div className="mainSplash2">Secondary Splash Here</div>
+                <div className="mainSplash2">
+Hi I am Vik! In my spare time I love to code. There is nothing more satisfying than a light bulb turning on in your head when you a solve a complicated piece of code or algorithm. In my future career I see myself working for a large tech firm in a team based environment. I love sharing ideas and also learning from my peers. They say team work makes the dream work which is why I picked the perfect field! My favourite language is React and I love to use Node.js for the backend. I began my career in sales but have now transitioned into the satisfying world of Software Engineering. When I am not coding you will find me doing a ton of other fun stuff. I love creating music and can play several instruments. I love Hollywood movies, Swimming and playing video games when I can.</div>
                 <div className="mainSplash3">Third Splash Here</div>
                 <div className="mainSplash4">Fourth Splash Here</div>
                 <div className="mainSplash5">Fifth Splash Here</div>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -13,9 +13,13 @@ class Portfolio extends Component {
                     <div className="mainSplash">Splash Content Here</div>
 
                 </div>
-                <div className="mainSplash2">Secondary Splash Here</div>
-                <div className="mainSplash3">Third Splash Here</div>
-                <div className="mainSplash4">Fourth Splash Here</div>
+                <div className="mainSplash2">Quiz App - This app is a random 5 question quiz app that tests your knowledge. A user can click on up to 4 answers and is given a recurring score and a final retry quiz page.
+Technologies: HTML, JavaScript, jQuery, CSS</div>
+                <div className="mainSplash3">NPMM - Node Package Manager Manager (NPMM) allows developers to frictionlessly browse and save npm packages. Saved packages can then be programmatically installed with our command line interface.
+Technologies: React, HTML5, CSS3, JavaScript, Redux, Node, PostgreSQL, Express, Mocha, Chai</div>
+                <div className="mainSplash4">POSTED - An online postcard generator that allows users to create postcards using a URL link and text of their choice.
+Technologies: React, Node.JS, PostgreSQL, Express, Mocha, Chai
+</div>
                 <div className="mainSplash5">Fifth Splash Here</div>
             </div>
         );
