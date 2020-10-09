e0abe4a875350b273abeb9691e897abc6e1bcd63
Vik Birdi <lifeasvik@gmail.com>
2020-10-09 16:15:28 -0500

    working on animations for main page

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,11 +9,15 @@ class Home extends Component {
             <div>
                 <SideBar pageWrapId={"page-wrap"} outerContainerId={"App"} />
                 <div><SideBar /></div>
-                <div className="pagePadder">
-                    <div className="mainSplash">Splash Content Here</div>
-
+                <div className="mainBox1">
+                    <div className="mainContent1">
+                        
+                    </div>
                 </div>
-                <div className="mainSplash2">Secondary Splash Here</div>
+                <div className="pagePadder, boxSizer">
+                    <div className="mainPageTitle"><h2>TEST ANIMATION VIK</h2></div>
+                </div>
+
                 <div className="mainSplash3">Third Splash Here</div>
                 <div className="mainSplash4">Fourth Splash Here</div>
                 <div className="mainSplash5">Fifth Splash Here</div>
