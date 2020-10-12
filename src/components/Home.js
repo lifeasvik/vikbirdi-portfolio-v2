c42112fb971c792d95987db66ffd924174f76048
Vik Birdi <lifeasvik@gmail.com>
2020-10-12 11:46:28 -0500

    added font worked on styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,18 +9,37 @@ class Home extends Component {
             <div>
                 <SideBar pageWrapId={"page-wrap"} outerContainerId={"App"} />
                 <div><SideBar /></div>
-                <div className="mainBox1">
+
+                <div className="homeBox">
+                    <div className="homeContent1"><h1>Image Here</h1></div>
+                    <div className="homeContent1"><h1>Meet Vik</h1></div>
+                    <div className="homeContent2"><h1>Challenging boundaries through innovation, perserverence and dedication.</h1></div>
+                </div>
+                {/* TEST CONTENT HERE */}
+
+                <div className="testAnimationContainer hidden">
+                    <div className="mainText"><div><h1>Vik Birdi</h1></div></div>
+                    <div className="testBox1 hidden"></div>
+                    <div className="testBox2"></div>
+                    <div className="testBox3 hidden"></div> 
+                    <div className="testBox4 hidden"></div>
+                </div>
+
+                {/* TEST CONTENT HERE */}
+
+                {/* <div className="mainBox1">
                     <div className="mainContent1">
-                        
+                        <div className="mainSplash"></div>
                     </div>
                 </div>
+
                 <div className="pagePadder, boxSizer">
                     <div className="mainPageTitle"><h2>TEST ANIMATION VIK</h2></div>
                 </div>
 
                 <div className="mainSplash3">Third Splash Here</div>
                 <div className="mainSplash4">Fourth Splash Here</div>
-                <div className="mainSplash5">Fifth Splash Here</div>
+                <div className="mainSplash5">Fifth Splash Here</div> */}
             </div>
         );
     }
