5cfe92ff1953a8f8e8afa168ec7e5ab82bab3874
Vik Birdi <lifeasvik@gmail.com>
2020-10-16 16:04:52 -0500

    added languages to wireframe and fixed projects for wireframe

--- src/components/Languages.js
+++ src/components/Languages.js
@@ -4,18 +4,31 @@ import SideBar from "./Sidebar";
 class Languages extends Component {
     render() {
         return (
-            <div><div><SideBar /></div>
-                <div className="pagePadder">
-                    <div><h1>Languages</h1></div>
+            <div>
+                <div>
+                    <SideBar />
                 </div>
-                <div className="pagePadder">
-                    <div className="mainSplash">Splash Content Here</div>
 
-                </div>
-                <div className="mainSplash2">Secondary Splash Here</div>
-                <div className="mainSplash3">Third Splash Here</div>
-                <div className="mainSplash4">Fourth Splash Here</div>
-                <div className="mainSplash5">Fifth Splash Here</div>
+                <div className="homeBox">
+
+                    <div className="homeContent1">
+                        <h1>Languages</h1>
+                    </div>
+
+                    <div className="homeContent1">
+                        <ul>
+                            <li>React.JS</li>
+                            <li>HTML5</li>
+                            <li>CSS3</li>
+                            <li>JavaScript</li>
+                            <li>Node.JS</li>
+                            <li>PostgreSQL</li>
+                            <li>MongoDB</li>
+                            <li>Python</li>
+                        </ul>
+                    </div>
+            
+                 </div>
             </div>
         );
     }
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,23 +5,59 @@ import SideBar from "./Sidebar";
 class Portfolio extends Component {
     render() {
         return (
-            <div><div><SideBar /></div>
-                <div className="pagePadder">
-                    <div><h1>Portfolio</h1></div>
+            <div>
+                <div>
+                    <SideBar />
                 </div>
-                <div className="pagePadder">
-                    <div className="mainSplash">Splash Content Here</div>
 
+                <div className="homeBox">
+
+                    <div className="homeContent1">
+                        <h1>Image Here</h1>
+                    </div>
+                    
+                    <div className="homeContent1">
+                        <h1>Learn About Me</h1>
+                    </div>
+                
+                    <div className="textShaper">
+                        <h3>Here are some examples of design work I have completed. There are some examples of great UX and UI designs where others demonstrate the backend and front end synergy. Please feel free to contact me if you would like to work on a project.</h3>
+                    </div>
+
+                    <div className="homeContent1">
+                        <div>
+                            <div>
+                                <h1>NPMM</h1>
+                            </div>
+
+                            <div>
+                                <p> Node Package Manager Manager (NPMM) allows developers to frictionlessly browse and save npm packages. Saved packages can then be programmatically installed with our command line interface. Technologies: React, HTML5, CSS3, JavaScript, Redux, Node, PostgreSQL, Express, Mocha, Chai Live App or GitHub Repo.</p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div className="homeContent1">
+                        <div>
+                            <h1>My Crazy Quiz App</h1>
+                        </div>
+
+                        <div>
+                            <p>This app is a random 5 question quiz app that tests your knowledge. A user can click on up to 4 answers and is given a recurring score and a final retry quiz page. Technologies: HTML, JavaScript, jQuery, CSS Live Quiz App or GitHub Repo</p>
+                        </div>
+                    </div>
+
+                    <div className="homeContent1">
+                        <div>
+                            <h1>POSTED</h1>
+                        </div>
+
+                        <div>
+                            <p>An online postcard generator that allows users to create postcards using a URL link and text of their choice. Technologies: React, Node.JS, PostgreSQL, Express, Mocha, Chai. Live App or GitHub Repo</p>
+                        </div>
+                    </div>
                 </div>
-                <div className="mainSplash2">Quiz App - This app is a random 5 question quiz app that tests your knowledge. A user can click on up to 4 answers and is given a recurring score and a final retry quiz page.
-Technologies: HTML, JavaScript, jQuery, CSS</div>
-                <div className="mainSplash3">NPMM - Node Package Manager Manager (NPMM) allows developers to frictionlessly browse and save npm packages. Saved packages can then be programmatically installed with our command line interface.
-Technologies: React, HTML5, CSS3, JavaScript, Redux, Node, PostgreSQL, Express, Mocha, Chai</div>
-                <div className="mainSplash4">POSTED - An online postcard generator that allows users to create postcards using a URL link and text of their choice.
-Technologies: React, Node.JS, PostgreSQL, Express, Mocha, Chai
-</div>
-                <div className="mainSplash5">Fifth Splash Here</div>
             </div>
+            
         );
     }
 }
